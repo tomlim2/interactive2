9445f7fd822497ec940f5053c2a6ec60fe593808
tomlim2 <tomlim@youns-MacBook-Pro.local>
2017-02-27 15:27:54 -0800

    Test22

---
+++ typo3pj3/assets/js/myscript.js
@@ -0,0 +1,98 @@
+$("#mvpoint1").click(function() {
+$('html, body').animate({
+  scrollLeft: $("#mvpoint2").offset().left
+}, 1200);
+
+});
+
+
+
+  $("#boxxx01").hover(function() {
+    $(".dc1").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx02").hover(function() {
+    $(".dc2").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx03").hover(function() {
+    $(".dc3").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx04").hover(function() {
+    $(".dc4").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx05").hover(function() {
+    $(".dc5").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx06").hover(function() {
+    $(".dc6").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx07").hover(function() {
+    $(".dc7").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx08").hover(function() {
+    $(".dc8").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx09").hover(function() {
+    $(".dc9").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx10").hover(function() {
+    $(".dc10").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx11").hover(function() {
+    $(".dc11").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx12").hover(function() {
+    $(".dc12").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx13").hover(function() {
+    $(".dc13").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx14").hover(function() {
+    $(".dc14").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx15").hover(function() {
+    $(".dc15").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx16").hover(function() {
+    $(".dc16").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx17").hover(function() {
+    $(".dc17").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
+
+  $("#boxxx18").hover(function() {
+    $(".dc18").toggleClass('appearmv');
+    $(".msimg1").toggleClass('op07mv');
+  });
