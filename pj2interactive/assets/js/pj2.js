ee3fd9129f9a86ab2c0a651edc07d8e9031b882b
tomlim2 <tomlim@youns-MacBook-Pro.local>
2017-03-09 09:01:33 -0800

    Project2end

--- pj2interactive/assets/js/pj2.js
+++ pj2interactive/assets/js/pj2.js
@@ -1,21 +1,46 @@
 $(document).ready(function() {
+$(".inputdeco2").click(function(){
+$(".interactiveframe").fadeIn("slow");
+});
 
 
+  var mySnow = ['a01', 'a02', 'a03', 'a04'];
 
-// $('#input').keypress(function(e) {
-//         $('#output').append(String.fromCharCode(e.which));
-//        // the keycode for 'a' is 97
-//         if ( e.which == 97 ) {
-//           $('#output').addClass('special');
-//         }
-// });
+  $(".inputdeco2").click(function(){
+    var rand = mySnow[Math.floor(Math.random() * mySnow.length)];
+
+    if(rand == 'a01'){
+      $(".msheet1").addClass("popup");
+      $(".msheet2").removeClass("popup");
+      $(".msheet3").removeClass("popup");
+      $(".msheet4").removeClass("popup");
+    }
+    if(rand == 'a02'){
+      $(".msheet1").removeClass("popup");
+      $(".msheet2").addClass("popup");
+      $(".msheet3").removeClass("popup");
+      $(".msheet4").removeClass("popup");
+    }
+    if(rand == 'a03'){
+      $(".msheet1").removeClass("popup");
+      $(".msheet2").removeClass("popup");
+      $(".msheet3").addClass("popup");
+      $(".msheet4").removeClass("popup");
+    }
+    if(rand == 'a04'){
+      $(".msheet1").removeClass("popup");
+      $(".msheet2").removeClass("popup");
+      $(".msheet3").removeClass("popup");
+      $(".msheet4").addClass("popup");
+    }
+    });
 
 
 $('body').keydown(function(e){
   if(e.keyCode == 192){
     $("#xl000a")[0].currentTime=0;
     $("#xl000a")[0].play();
-    $(".splttexm").toggleClass('blinkmv');
+    $(".intrsplttexm").toggleClass('blinkmv');
   }
 });
 
@@ -23,287 +48,287 @@ $('body').keydown(function(e){
   if(e.keyCode == 49){
     $("#xl00a")[0].currentTime=0;
     $("#xl00a")[0].play();
-    $(".splttq").toggleClass('blinkmv');
+    $(".intrsplttq").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 81){
     $("#xl01")[0].currentTime=0;
     $("#xl01")[0].play();
-    $(".splttq").toggleClass('blinkmv');
+    $(".intrsplttq").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 50){
     $("#xl01a")[0].currentTime=0;
     $("#xl01a")[0].play();
-    $(".spltt2").toggleClass('blinkmv');
+    $(".intrspltt2").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 87){
     $("#xl02")[0].currentTime=0;
     $("#xl02")[0].play();
-    $(".splttw").toggleClass('blinkmv');
+    $(".intrsplttw").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 51){
     $("#xl000")[0].currentTime=0;
     $("#xl000")[0].play();
-    $(".spltt3").toggleClass('blinkmv');
+    $(".intrspltt3").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 69){
     $("#xl03")[0].currentTime=0;
     $("#xl03")[0].play();
-    $(".spltte").toggleClass('blinkmv');
+    $(".intrspltte").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 52){
     $("#xl03a")[0].currentTime=0;
     $("#xl03a")[0].play();
-    $(".spltt4").toggleClass('blinkmv');
+    $(".intrspltt4").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 82){
     $("#xl04")[0].currentTime=0;
     $("#xl04")[0].play();
-    $(".splttr").toggleClass('blinkmv');
+    $(".intrsplttr").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 53){
     $("#xl04a")[0].currentTime=0;
     $("#xl04a")[0].play();
-    $(".spltt5").toggleClass('blinkmv');
+    $(".intrspltt5").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 84){
     $("#xl05")[0].currentTime=0;
     $("#xl05")[0].play();
-    $(".splttt").toggleClass('blinkmv');
+    $(".intrsplttt").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 54){
     $("#xl00")[0].currentTime=0;
     $("#xl00")[0].play();
-    $(".spltt6").toggleClass('blinkmv');
+    $(".intrspltt6").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 89){
     $("#xl06")[0].currentTime=0;
     $("#xl06")[0].play();
-    $(".spltty").toggleClass('blinkmv');
+    $(".intrspltty").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 55){
     $("#xl06a")[0].currentTime=0;
     $("#xl06a")[0].play();
-    $(".spltt7").toggleClass('blinkmv');
+    $(".intrspltt7").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 85){
     $("#xl07")[0].currentTime=0;
     $("#xl07")[0].play();
-    $(".splttu").toggleClass('blinkmv');
+    $(".intrsplttu").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 56){
     $("#xl07a")[0].currentTime=0;
     $("#xl07a")[0].play();
-    $(".spltt8").toggleClass('blinkmv');
+    $(".intrspltt8").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 73){
     $("#xl08")[0].currentTime=0;
     $("#xl08")[0].play();
-    $(".spltti").toggleClass('blinkmv');
+    $(".intrspltti").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 57){
     $("#xl08a")[0].currentTime=0;
     $("#xl08a")[0].play();
-    $(".spltt9").toggleClass('blinkmv');
+    $(".intrspltt9").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 79){
     $("#xl09")[0].currentTime=0;
     $("#xl09")[0].play();
-    $(".spltto").toggleClass('blinkmv');
+    $(".intrspltto").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 48){
     $("#xl00")[0].currentTime=0;
     $("#xl00")[0].play();
-    $(".spltt0").toggleClass('blinkmv');
+    $(".intrspltt0").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 80){
     $("#xl10")[0].currentTime=0;
     $("#xl10")[0].play();
-    $(".splttp").toggleClass('blinkmv');
+    $(".intrsplttp").toggleClass('blinkmv');
   }
 });
 $('body').keydown(function(e){
   if(e.keyCode == 65){
     $("#xl10a")[0].currentTime=0;
     $("#xl10a")[0].play();
-    $(".spltta").toggleClass('blinkmv');
+    $(".intrspltta").toggleClass('blinkmv');
   }
 });
   $('body').keydown(function(e){
     if(e.keyCode == 68){
       $("#xl03")[0].currentTime=0;
       $("#xl03")[0].play();
-      $(".splttd").toggleClass('blinkmv');
+      $(".intrsplttd").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 90){
       $("#xl11")[0].currentTime=0;
       $("#xl11")[0].play();
-      $(".splttz").toggleClass('blinkmv');
+      $(".intrsplttz").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 83){
       $("#xl11a")[0].currentTime=0;
       $("#xl11a")[0].play();
-      $(".spltts").toggleClass('blinkmv');
+      $(".intrspltts").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 88){
       $("#xl12")[0].currentTime=0;
       $("#xl12")[0].play();
-      $(".splttx").toggleClass('blinkmv');
+      $(".intrsplttx").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 67){
       $("#xl13")[0].currentTime=0;
       $("#xl13")[0].play();
-      $(".splttc").toggleClass('blinkmv');
+      $(".intrsplttc").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 70){
       $("#xl13a")[0].currentTime=0;
       $("#xl13a")[0].play();
-      $(".splttf").toggleClass('blinkmv');
+      $(".intrsplttf").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 86){
       $("#xl14")[0].currentTime=0;
       $("#xl14")[0].play();
-      $(".splttv").toggleClass('blinkmv');
+      $(".intrsplttv").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 71){
       $("#xl14a")[0].currentTime=0;
       $("#xl14a")[0].play();
-      $(".splttg").toggleClass('blinkmv');
+      $(".intrsplttg").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 66){
       $("#xl15")[0].currentTime=0;
       $("#xl15")[0].play();
-      $(".splttb").toggleClass('blinkmv');
+      $(".intrsplttb").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 72){
       $("#xl15a")[0].currentTime=0;
       $("#xl15a")[0].play();
-      $(".spltth").toggleClass('blinkmv');
+      $(".intrspltth").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 78){
       $("#xl16")[0].currentTime=0;
       $("#xl16")[0].play();
-      $(".splttn").toggleClass('blinkmv');
+      $(".intrsplttn").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 74){
       $("#xl000")[0].currentTime=0;
       $("#xl000")[0].play();
-      $(".splttj").toggleClass('blinkmv');
+      $(".intrsplttj").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 77){
       $("#xl17")[0].currentTime=0;
       $("#xl17")[0].play();
-      $(".splttm").toggleClass('blinkmv');
+      $(".intrsplttm").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 75){
       $("#xl17a")[0].currentTime=0;
       $("#xl17a")[0].play();
-      $(".splttk").toggleClass('blinkmv');
+      $(".intrsplttk").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 188){
       $("#xl18")[0].currentTime=0;
       $("#xl18")[0].play();
-      $(".splttrest").toggleClass('blinkmv');
+      $(".intrsplttrest").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 76){
       $("#xl18a")[0].currentTime=0;
       $("#xl18a")[0].play();
-      $(".splttl").toggleClass('blinkmv');
+      $(".intrsplttl").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 190){
       $("#xl19")[0].currentTime=0;
       $("#xl19")[0].play();
-      $(".splttstp").toggleClass('blinkmv');
+      $(".intrsplttstp").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 191){
       $("#xl20")[0].currentTime=0;
       $("#xl20")[0].play();
-      $(".splttqmk").toggleClass('blinkmv');
+      $(".intrsplttqmk").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 186){
       $("#xl000")[0].currentTime=0;
       $("#xl000")[0].play();
-      $(".splttom").toggleClass('blinkmv');
+      $(".intrsplttom").toggleClass('blinkmv');
     }
   });
   $('body').keydown(function(e){
     if(e.keyCode == 222){
       $("#xl00")[0].currentTime=0;
       $("#xl00")[0].play();
-      $(".splttcm").toggleClass('blinkmv');
+      $(".intrsplttcm").toggleClass('blinkmv');
     }
   });
 
--- pj2show/assets/js/pj2.js
+++ pj2show/assets/js/pj2.js
@@ -3,6 +3,28 @@ $(document).ready(function() {
 // first snow start
 
   $(".sfbaseframe1").click(function(){
+    setTimeout(function(){
+      $("#xl08")[0].play();
+    },0000);
+    setTimeout(function(){
+      $("#xl09")[0].play();
+    },2800);
+    setTimeout(function(){
+      $("#xl10")[0].play();
+    },3500);
+    setTimeout(function(){
+      $("#xl11")[0].play();
+    },4200);
+    setTimeout(function(){
+      $("#xl12")[0].play();
+    },4900);
+    setTimeout(function(){
+      $("#xl13")[0].play();
+    },5600);
+    setTimeout(function(){
+      $("#xl14")[0].play();
+    },6300);
+
     $(".bgccr").addClass('bgccrmv');
 
     $(".sfbaseframe12").addClass('spfa02mv');
@@ -14,7 +36,7 @@ $(document).ready(function() {
     $(".sfbaseframe1").removeClass('spfa00mv');
     $(".sfbaseframe1").addClass('spfa01mv');
 
-    $(".sfbaseframe0").addClass('revealmv');
+    $(".bigsnow1").addClass('revealmv');
 
     $(".fsspltta").addClass('spfsmv1');
     $(".fssplttb").addClass('spfsmv2');
@@ -65,11 +87,27 @@ $(document).ready(function() {
 
 // first snow end
 
+// second snow start
 
-  $(".sfbaseframe0").click(function(){
+  $(".bigsnow1").click(function(){
+    setTimeout(function(){
+      $("#xl16")[0].play();
+    },0000);
+
+      $(".ssbaseframe0").removeClass('spfa00mv');
+      $(".ssbaseframe0").addClass('clickeffmv');
+      $(".firstsnowframe").addClass('hidden');
+      setTimeout(function(){
+        $(".ssallctrl").fadeOut("slow");
+      },5500);
+      setTimeout(function(){
+        $(".showframe").fadeOut("slow");
+      },7000);
+      setTimeout(function(){
+        $(".inputbox01").fadeIn("slow");
+      },8000);
+      // $(".showframe").addClass('showframemv');
     // pack2
-    $(".shells").addClass('shellsmv');
-
       $(".aspltta").addClass('asnowmvr1');
       $(".asplttb").addClass('asnowmvr2');
       $(".asplttc").addClass('asnowmvr3');
@@ -115,11 +153,364 @@ $(document).ready(function() {
       $(".aspltt9").addClass('asnowmvr36');
       $(".asplttstp").addClass('asnowmvr37');
       $(".asplttexm").addClass('asnowmvr38');
-      $(".asplttx").addClass('asnowmvr39');
-      $(".aspltty").addClass('asnowmvr40');
-
+      $(".asplttqmk").addClass('asnowmvr39');
+      $(".asplttrest").addClass('asnowmvr40');
+      $(".asplttom").addClass('asnowmvr41');
+      $(".asplttcm").addClass('asnowmvr42');
     // console.log(rand);
   });
 
+
+
+// interactive part start////////////////////////////////
+
+$(".inputdeco2").click(function(){
+$(".interactiveframe").fadeIn("slow");
+$(".firstsnowframe").addClass("hidden");
+$(".showframe").addClass("hidden");
+});
+
+
+  var mySnow = ['a01', 'a02', 'a03', 'a04'];
+
+  $(".inputdeco2").click(function(){
+    var rand = mySnow[Math.floor(Math.random() * mySnow.length)];
+
+    if(rand == 'a01'){
+      $(".msheet1").addClass("popup");
+      $(".msheet2").removeClass("popup");
+      $(".msheet3").removeClass("popup");
+      $(".msheet4").removeClass("popup");
+      $("#xl17")[0].currentTime=0;
+      $("#xl17")[0].play();
+      $(".intrsplttm").toggleClass('blinkmv');
+    }
+    if(rand == 'a02'){
+      $(".msheet1").removeClass("popup");
+      $(".msheet2").addClass("popup");
+      $(".msheet3").removeClass("popup");
+      $(".msheet4").removeClass("popup");
+      $("#xl16")[0].currentTime=0;
+      $("#xl16")[0].play();
+      $(".intrsplttn").toggleClass('blinkmv');
+    }
+    if(rand == 'a03'){
+      $(".msheet1").removeClass("popup");
+      $(".msheet2").removeClass("popup");
+      $(".msheet3").addClass("popup");
+      $(".msheet4").removeClass("popup");
+      $("#xl15")[0].currentTime=0;
+      $("#xl15")[0].play();
+      $(".intrsplttb").toggleClass('blinkmv');
+    }
+    if(rand == 'a04'){
+      $(".msheet1").removeClass("popup");
+      $(".msheet2").removeClass("popup");
+      $(".msheet3").removeClass("popup");
+      $(".msheet4").addClass("popup");
+      $("#xl14")[0].currentTime=0;
+      $("#xl14")[0].play();
+      $(".intrsplttv").toggleClass('blinkmv');
+    }
+    });
+
+
+
+
+    $('body').keydown(function(e){
+      if(e.keyCode == 192){
+        $("#xl000a")[0].currentTime=0;
+        $("#xl000a")[0].play();
+        $(".intrsplttexm").toggleClass('blinkmv');
+      }
+    });
+
+    $('body').keydown(function(e){
+      if(e.keyCode == 49){
+        $("#xl00a")[0].currentTime=0;
+        $("#xl00a")[0].play();
+        $(".intrsplttq").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 81){
+        $("#xl01")[0].currentTime=0;
+        $("#xl01")[0].play();
+        $(".intrsplttq").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 50){
+        $("#xl01a")[0].currentTime=0;
+        $("#xl01a")[0].play();
+        $(".intrspltt2").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 87){
+        $("#xl02")[0].currentTime=0;
+        $("#xl02")[0].play();
+        $(".intrsplttw").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 51){
+        $("#xl000")[0].currentTime=0;
+        $("#xl000")[0].play();
+        $(".intrspltt3").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 69){
+        $("#xl03")[0].currentTime=0;
+        $("#xl03")[0].play();
+        $(".intrspltte").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 52){
+        $("#xl03a")[0].currentTime=0;
+        $("#xl03a")[0].play();
+        $(".intrspltt4").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 82){
+        $("#xl04")[0].currentTime=0;
+        $("#xl04")[0].play();
+        $(".intrsplttr").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 53){
+        $("#xl04a")[0].currentTime=0;
+        $("#xl04a")[0].play();
+        $(".intrspltt5").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 84){
+        $("#xl05")[0].currentTime=0;
+        $("#xl05")[0].play();
+        $(".intrsplttt").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 54){
+        $("#xl00")[0].currentTime=0;
+        $("#xl00")[0].play();
+        $(".intrspltt6").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 89){
+        $("#xl06")[0].currentTime=0;
+        $("#xl06")[0].play();
+        $(".intrspltty").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 55){
+        $("#xl06a")[0].currentTime=0;
+        $("#xl06a")[0].play();
+        $(".intrspltt7").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 85){
+        $("#xl07")[0].currentTime=0;
+        $("#xl07")[0].play();
+        $(".intrsplttu").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 56){
+        $("#xl07a")[0].currentTime=0;
+        $("#xl07a")[0].play();
+        $(".intrspltt8").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 73){
+        $("#xl08")[0].currentTime=0;
+        $("#xl08")[0].play();
+        $(".intrspltti").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 57){
+        $("#xl08a")[0].currentTime=0;
+        $("#xl08a")[0].play();
+        $(".intrspltt9").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 79){
+        $("#xl09")[0].currentTime=0;
+        $("#xl09")[0].play();
+        $(".intrspltto").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 48){
+        $("#xl00")[0].currentTime=0;
+        $("#xl00")[0].play();
+        $(".intrspltt0").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 80){
+        $("#xl10")[0].currentTime=0;
+        $("#xl10")[0].play();
+        $(".intrsplttp").toggleClass('blinkmv');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode == 65){
+        $("#xl10a")[0].currentTime=0;
+        $("#xl10a")[0].play();
+        $(".intrspltta").toggleClass('blinkmv');
+      }
+    });
+      $('body').keydown(function(e){
+        if(e.keyCode == 68){
+          $("#xl03")[0].currentTime=0;
+          $("#xl03")[0].play();
+          $(".intrsplttd").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 90){
+          $("#xl11")[0].currentTime=0;
+          $("#xl11")[0].play();
+          $(".intrsplttz").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 83){
+          $("#xl11a")[0].currentTime=0;
+          $("#xl11a")[0].play();
+          $(".intrspltts").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 88){
+          $("#xl12")[0].currentTime=0;
+          $("#xl12")[0].play();
+          $(".intrsplttx").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 67){
+          $("#xl13")[0].currentTime=0;
+          $("#xl13")[0].play();
+          $(".intrsplttc").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 70){
+          $("#xl13a")[0].currentTime=0;
+          $("#xl13a")[0].play();
+          $(".intrsplttf").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 86){
+          $("#xl14")[0].currentTime=0;
+          $("#xl14")[0].play();
+          $(".intrsplttv").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 71){
+          $("#xl14a")[0].currentTime=0;
+          $("#xl14a")[0].play();
+          $(".intrsplttg").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 66){
+          $("#xl15")[0].currentTime=0;
+          $("#xl15")[0].play();
+          $(".intrsplttb").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 72){
+          $("#xl15a")[0].currentTime=0;
+          $("#xl15a")[0].play();
+          $(".intrspltth").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 78){
+          $("#xl16")[0].currentTime=0;
+          $("#xl16")[0].play();
+          $(".intrsplttn").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 74){
+          $("#xl000")[0].currentTime=0;
+          $("#xl000")[0].play();
+          $(".intrsplttj").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 77){
+          $("#xl17")[0].currentTime=0;
+          $("#xl17")[0].play();
+          $(".intrsplttm").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 75){
+          $("#xl17a")[0].currentTime=0;
+          $("#xl17a")[0].play();
+          $(".intrsplttk").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 188){
+          $("#xl18")[0].currentTime=0;
+          $("#xl18")[0].play();
+          $(".intrsplttrest").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 76){
+          $("#xl18a")[0].currentTime=0;
+          $("#xl18a")[0].play();
+          $(".intrsplttl").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 190){
+          $("#xl19")[0].currentTime=0;
+          $("#xl19")[0].play();
+          $(".intrsplttstp").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 191){
+          $("#xl20")[0].currentTime=0;
+          $("#xl20")[0].play();
+          $(".intrsplttqmk").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 186){
+          $("#xl000")[0].currentTime=0;
+          $("#xl000")[0].play();
+          $(".intrsplttom").toggleClass('blinkmv');
+        }
+      });
+      $('body').keydown(function(e){
+        if(e.keyCode == 222){
+          $("#xl00")[0].currentTime=0;
+          $("#xl00")[0].play();
+          $(".intrsplttcm").toggleClass('blinkmv');
+        }
+      });
 
 });
